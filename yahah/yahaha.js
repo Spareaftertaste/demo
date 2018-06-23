465bed217c7b5a4acc240a1887a2c0a233268e91
Spareaftertaste <2383860945@qq.com>
2018-06-23 20:27:24 +0800

    轮播图

---
+++ yahah/yahaha.js
@@ -0,0 +1,19 @@
+var miniRefresh = new MiniRefresh({
+    container: '#minirefresh',
+    down: {
+        callback: function() {
+            // 下拉事件
+
+            miniRefresh.endDownLoading();
+        }
+    },
+    up: {
+        isAuto: true,
+        callback: function() {
+            // 上拉事件
+
+            // 注意，由于默认情况是开启满屏自动加载的，所以请求失败时，请务必endUpLoading(true)，防止无限请求
+            miniRefresh.endUpLoading(true);
+        }
+    }
+});
