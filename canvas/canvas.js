6905bc4c1d833d1c527372889c64a5084671630b
Spareaftertaste <2383860945@qq.com>
2018-06-24 11:46:22 +0800

    波浪线思密达

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -22,12 +22,12 @@ function loop(){
     for(var j = lines.length - 1; j >= 0; j--) {
         ctx.fillStyle = lines[j];
         //每个矩形的角度都不同，每个之间相差45度
-        var angle = (step+j*45)*Math.PI/180;
-        var deltaHeight   = Math.sin(angle) * 20;
-        var deltaHeightRight   = Math.cos(angle) * 20;
+        var angle = (step+j*90)*Math.PI/180;
+        var deltaHeight   = Math.cos(angle) * 15;
+        var deltaHeightRight   = Math.sin(angle) * 15;
         ctx.beginPath();
         ctx.moveTo(0, canvas.height*9/10);
-        ctx.bezierCurveTo(canvas.width /2, canvas.height*9/10+deltaHeight-50, canvas.width / 2, canvas.height*9/10+deltaHeightRight-50, canvas.width, canvas.height*9/10+deltaHeightRight);
+        ctx.bezierCurveTo(canvas.width /2, canvas.height*9/10+deltaHeight-30, canvas.width / 2, canvas.height*9/10+deltaHeightRight-30, canvas.width, canvas.height*9/10+deltaHeightRight);
         ctx.lineTo(canvas.width, canvas.height);
         ctx.lineTo(0, canvas.height);
         ctx.lineTo(0, canvas.height/2+deltaHeight);
